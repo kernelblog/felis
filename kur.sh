b9bbfd92c21e9a42c982d24778c7bbd4cdceda88
frknkrc44 <krc440002@gmail.com>
2019-09-25 19:46:02 +0300

    Clean script

--- kur.sh
+++ kur.sh
@@ -22,25 +22,10 @@ fi
 
 exec PIP_CMD install beautifulsoup4 requests PrettyTable youtube_dl
 
-#sudo apt-get install python3
-#sudo apt-get install python3-pip
-#sudo apt-get install git
-#sudo pip3 install beautifulsoup4
-#sudo pip3 install requests
-#sudo pip3 install PrettyTable
-#sudo pip3 install youtube_dl
-sudo mkdir /usr/share/felis
-sudo cp felis /usr/bin/
-sudo cp felis.py /usr/share/felis/
-sudo cp mp.py /usr/share/felis/
-sudo cp githubmodul.py /usr/share/felis/
-sudo cp distrorepo.py /usr/share/felis/
-sudo cp guncelleme.py /usr/share/felis/
-sudo chmod 755 /usr/bin/felis
-sudo chmod 755 /usr/share/felis/felis.py
-sudo chmod 755 /usr/share/felis/githubmodul.py
-sudo chmod 755 /usr/share/felis/mp.py
-sudo chmod 755 /usr/share/felis/guncelleme.py
-sudo chmod 755 /usr/share/felis/distrorepo.py
+mkdir /usr/share/felis
+cp felis /usr/bin/
+cp *.py /usr/share/felis/
+chmod 755 /usr/bin/felis
+chmod 755 /usr/share/felis/*
 
 echo "Yükleme başarıyla tamamlandı. Terminale felis yazarak kullanabilirsiniz."
