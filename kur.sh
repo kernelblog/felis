02841eef14986cd67cab912df0d216f46babea6e
frknkrc44 <krc440002@gmail.com>
2019-09-25 19:40:31 +0300

    Add termux support (needs testing) and remove sudo

--- kur.sh
+++ kur.sh
@@ -1,10 +1,34 @@
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo apt-get install git
-sudo pip3 install beautifulsoup4
-sudo pip3 install requests
-sudo pip3 install PrettyTable
-sudo pip3 install youtube_dl
+if [ $(id -g) -ne 0 ]
+then
+	echo "Bu komutu sadece root erişimiyle çalıştırabilirsiniz"
+	exit 1
+fi
+
+if [ -f /usr/bin/apt ] || [ -f $PREFIX/bin/apt ]
+then
+	apt update
+	apt install python3 python3-pip git
+fi
+
+PIP_CMD="pip3"
+
+if [ -f /usr/bin/pip3 ]
+then
+	PIP_CMD="/usr/bin/pip3"
+elif [ -f $PREFIX/bin/pip ]
+then
+	PIP_CMD=$PREFIX"/bin/pip"
+fi
+
+exec PIP_CMD install beautifulsoup4 requests PrettyTable youtube_dl
+
+#sudo apt-get install python3
+#sudo apt-get install python3-pip
+#sudo apt-get install git
+#sudo pip3 install beautifulsoup4
+#sudo pip3 install requests
+#sudo pip3 install PrettyTable
+#sudo pip3 install youtube_dl
 sudo mkdir /usr/share/felis
 sudo cp felis /usr/bin/
 sudo cp felis.py /usr/share/felis/
