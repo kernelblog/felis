037a9fc4885f3930aba48783678ed83ef844b825
GhostRoot <31356491+GhstRt@users.noreply.github.com>
2019-09-25 21:26:33 +0300

    Update kur.sh

--- kur.sh
+++ kur.sh
@@ -1,22 +1,31 @@
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo apt-get install git
-sudo pip3 install beautifulsoup4
-sudo pip3 install requests
-sudo pip3 install PrettyTable
-sudo pip3 install youtube_dl
-sudo mkdir /usr/share/felis
-sudo cp felis /usr/bin/
-sudo cp felis.py /usr/share/felis/
-sudo cp mp.py /usr/share/felis/
-sudo cp githubmodul.py /usr/share/felis/
-sudo cp distrorepo.py /usr/share/felis/
-sudo cp guncelleme.py /usr/share/felis/
-sudo chmod 755 /usr/bin/felis
-sudo chmod 755 /usr/share/felis/felis.py
-sudo chmod 755 /usr/share/felis/githubmodul.py
-sudo chmod 755 /usr/share/felis/mp.py
-sudo chmod 755 /usr/share/felis/guncelleme.py
-sudo chmod 755 /usr/share/felis/distrorepo.py
+if [ $(id -g) -ne 0 ] || [ ! -n $PREFIX ]
+then
+        echo "Bu komutu sadece root erişimiyle çalıştırabilirsiniz"
+        exit 1
+fi
+
+if [ -f /usr/bin/apt ] || [ -f $PREFIX/bin/apt ]
+then
+        apt update
+        apt install python3 python3-pip git
+fi
+
+PIP_CMD="pip3"
+
+if [ -f /usr/bin/pip3 ]
+then
+        PIP_CMD="/usr/bin/pip3"
+elif [ -f $PREFIX/bin/pip ]
+then
+        PIP_CMD=$PREFIX"/bin/pip"
+fi
+
+$PIP_CMD install beautifulsoup4 requests PrettyTable youtube_dl
+
+mkdir /usr/share/felis
+cp felis /usr/bin/
+cp *.py /usr/share/felis/
+chmod 755 /usr/bin/felis
+chmod 755 /usr/share/felis/*
 
 echo "Yükleme başarıyla tamamlandı. Terminale felis yazarak kullanabilirsiniz."
